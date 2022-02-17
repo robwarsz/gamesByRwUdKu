d88abf805592b8a3cb49a2581805f3f5bad2e163
robwarsz <robertwarszawski@gmail.com>
2022-02-17 18:15:44 +0100

    Initial commit

--- test2.py
+++ test2.py
@@ -1,6 +1,8 @@
 import pygame
 
 pygame.init()
+clock = pygame.time.Clock()
+print(clock)
 
 screen = pygame.display.set_mode((800,600))
 
@@ -17,8 +19,20 @@ playerX = 368
 playerY = 480
 speed = 0
 
+#shoot
+shootImg = pygame.image.load("assets/shoot.png")
+shootX=400
+shootY=400
+shootSpeed=10
+shootState = "ready"
+
 def player(x,y):
     screen.blit(playerImg,(x,y))
+
+def shootFromShip(x,y):
+    global shootState
+    shootState = "throw"
+    screen.blit(shootImg,(x,y-40))
 
 running = True
 while running:
@@ -26,16 +40,32 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT:
+        #         speed = -0.1
+        #         #print(playerX,playerY)
+        #     if event.key == pygame.K_RIGHT:
+        #         speed = 0.1
+        #         #print(playerX,playerY)
+
+        # if event.type == pygame.KEYUP:
+        #     if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        #         speed = 0
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                speed = -0.1
-                #print(playerX,playerY)
-            if event.key == pygame.K_RIGHT:
-                speed = 0.1
-                #print(playerX,playerY)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                speed = 0
+            if event.key == pygame.K_SPACE:
+                print("SPACE")
+                bulletY=playerY
+                shootFromShip(playerX,bulletY)
+
+        keys = pygame.key.get_pressed()
+
+        speed=0
+        if keys[pygame.K_LEFT]:
+            speed = -1
+        elif keys[pygame.K_RIGHT]:
+            speed = 1
+
+
 
     playerX+=speed
 
@@ -45,4 +75,9 @@ while running:
        playerX=736
 
     player(playerX,playerY)
-    pygame.display.update()
+    if shootState is "throw":
+        shootFromShip(playerX,bulletY)
+        bulletY-=shootSpeed
+
+    pygame.display.flip()
+    clock.tick(60)
